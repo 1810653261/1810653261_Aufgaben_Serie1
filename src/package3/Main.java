a1e66abfb08bd3e27e14f224d3a1840aaaca94e2
Joachimstaller Alexander Lothar <1810653261@stud.fh-kufstein.ac.at>
2018-11-08 14:59:16 +0100

    Auskommentierte Aufgaben 1-5

--- src/package3/Main.java
+++ src/package3/Main.java
@@ -1,18 +1,18 @@
-package package3;
+package package3;//neues Package erstellt mit rechtsklick auf src->new->package
 
 
-import javax.print.DocFlavor;
+
 
 public class Main {
-    public static void main(String[] args) {
-        String name=("Alex");
-        System.out.println(" " + name);
-        int alter=(18);
-        System.out.println(" " + alter);
+    public static void main(String[] args) { //main Methode erstellt mit public static void main...
+        String name=("Alex");// Durch eine string variable "name" meinen Namen ausgegeben
+        System.out.println(" " + name);// Meinen namen durch die variable ausgegeben
+        int alter=(18);//mit einer variable int mein alter als varible gesetzt
+        System.out.println(" " + alter);//Mein alter mit hilfe einer Variable ausgegeben
 
         final String FH_KUFSTEIN_TIROL=("Fachhochschule Kufstein Tirol");// Variablenname von FachhochschuleKufsteinTirol auf fhKufsteinTirol verkürzt und durch das Schlüsselwort Final das wort verkürzt
 
-        final String Wohnort=("Rosenheim");
+        final String Wohnort=("Rosenheim"); // Erneut einen String mit schlüsselwort final und einer nicht vorgegebenen variablen
 
 
         System.out.println("JavaverwendetfürStrings\"doppelteAnführungszeichen\"undeinzelneAnführungszeichen\n" +
