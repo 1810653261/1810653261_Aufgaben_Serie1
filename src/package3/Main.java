2a18e279df28df4b5021fb51210c01f0a02f6de2
Joachimstaller Alexander Lothar <1810653261@stud.fh-kufstein.ac.at>
2018-11-08 14:17:28 +0100

    drittes Package + Main Methode + Name und Alter ausgeben

---
+++ src/package3/Main.java
@@ -0,0 +1,12 @@
+package package3;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String name=("Alex");
+        System.out.println(" " + name);
+        int alter=(18);
+        System.out.println(" " + alter);
+    }
+}
