f3f8de748c9b4725450318f4e6310384a0c9a25e
Joachimstaller Alexander Lothar <1810653261@stud.fh-kufstein.ac.at>
2018-11-08 14:03:13 +0100

    Kommentare Überarbeitet

--- src/package2/fh_kufstein.java
+++ src/package2/fh_kufstein.java
@@ -1,6 +1,6 @@
-package package2;//neues Package erstellt
+package package2;//neues Package erstellt mit rechtsklick auf src->new->package
 
-public class fh_kufstein { //neue Klasse erstellt
+public class fh_kufstein { //neue Klasse erstellt mit rechtsklick auf src->new->Java Class
     public static void main(String[] args) { // main Methode erstellt
         System.out.println("FH Kufstein Tirol"); // FH Kufstein Tirol ausgegeben
     }
