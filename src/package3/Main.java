944ef803c602567e64bf889f4665a6b12d3e6bb7
Joachimstaller Alexander Lothar <1810653261@stud.fh-kufstein.ac.at>
2018-11-08 14:54:10 +0100

    Escape Sequenzen hinzugefügt

--- src/package3/Main.java
+++ src/package3/Main.java
@@ -10,9 +10,22 @@ public class Main {
         int alter=(18);
         System.out.println(" " + alter);
 
-        final String FH_KUFSTEIN_TIROL=("Fachhochschule Kufstein Tirol")// Variablenname von FachhochschuleKufsteinTirol auf fhKufsteinTirol verkürzt und durch das Schlüsselwort Final das wort verkürzt
+        final String FH_KUFSTEIN_TIROL=("Fachhochschule Kufstein Tirol");// Variablenname von FachhochschuleKufsteinTirol auf fhKufsteinTirol verkürzt und durch das Schlüsselwort Final das wort verkürzt
 
         final String Wohnort=("Rosenheim");
+
+
+        System.out.println("JavaverwendetfürStrings\"doppelteAnführungszeichen\"undeinzelneAnführungszeichen\n" +
+                "wie 'c' um einzelne Zeichen zu definieren.");
+
+        System.out.println("Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\");
+
+        System.out.println("Ich verwende den\\b Backspace, die Newline\\n und den \\tTabulator.");
+
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \\r überschreibt\n" +
+                "diebestehendeZeile.\\fistinderKonsolenichtbemerkbar,daeseinenSeitenumbruch\n" +
+                "erzwingt.");
+
 
 
 
